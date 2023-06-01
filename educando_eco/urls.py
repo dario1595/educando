625dd1db5b1abf6cea238243d900d4fecc2e33e4
Dario Diaz <dariodiaz1595@gmail.com>
2023-06-01 14:11:53 -0300

    Fix

--- educando_eco/urls.py
+++ educando_eco/urls.py
@@ -1,27 +1,21 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from educando_eco import views
 
 router = routers.DefaultRouter()
-router.register(r'Categoria', views.CategoriaViewSet,'categoria')
-router.register(r'Curso', views.CursoViewSet,'curso')
-router.register(r'Carrito', views.CarritoViewSet,'carrito')
-router.register(r'Foro', views.ForoViewSet,'foro')
-router.register(r'Contacto', views.ContactoViewSet,'contacto')
+router.register(r'Categoria', views.CategoriaViewSet, basename='categoria')
+router.register(r'Curso', views.CursoViewSet, basename='curso')
+router.register(r'Carrito', views.CarritoViewSet, basename='carrito')
+router.register(r'Foro', views.ForoViewSet, basename='foro')
+router.register(r'Contacto', views.ContactoViewSet, basename='contacto')
+
 
 urlpatterns = [
-    *router.urls,  # Se incluyen las URLs generadas por el enrutador
-
-    # URL para el registro de usuarios
-    path('Registro/', views.UsuarioView.as_view(), name='registro'),
-
-    # URL para el inicio de sesión de usuarios
-    path('Login/', views.UsuarioView.inicio_sesion, name='login'),
-
-    # URL para obtener la lista de usuarios
-    path('Usuarios/', views.UsuarioView.lista_usuarios, name='lista_usuarios'),
+    path('', include(router.urls)),
+    path('Login/', views.UsuarioView.as_view({'post': 'inicio_sesion'}), name='login'),
+    path('Registro/', views.UsuarioView.as_view({'post': 'create_user'}), name='crear_usuario'),
+    path('Usuarios/', views.UsuarioView.as_view({'get': 'list_users'}), name='lista_usuarios'),
 
     path('Mis_cursos/', views.MisCursosView.as_view(), name='mis_cursos'),
-
-    path('Adquirir_curso/', views.AdquirirCursoView.as_view(), name='adquirir_curso')
+    path('Adquirir_curso/', views.AdquirirCursoView.as_view(), name='adquirir_curso'),
 ]
--- educando_eco/views.py
+++ educando_eco/views.py
@@ -1,5 +1,3 @@
-
-
 from .serializer import  UsuarioSerializer, CategoriaSerializer, CursoSerializer, MisCursoSerializer, CarritoSerializer, ForoSerializer, ContactoSerializer
 from .models import  Usuario, Categoria,Curso, MisCurso, Carrito, Foro, Contacto
 
@@ -11,61 +9,55 @@ from rest_framework.exceptions import AuthenticationFailed
 
 from django.utils import timezone
 from django.conf import settings
-from django.http import JsonResponse
 from django.contrib.auth import authenticate
-from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 
-import json, datetime, jwt
+import datetime, jwt
 
-class UsuarioView(APIView):
-    @csrf_exempt
-    def post(self, request):
-        # Se recibe una solicitud POST con los datos del usuario a registrar
+class UsuarioView(viewsets.ViewSet):
+    permission_classes = [AllowAny]
+
+    def create_user(self, request):
         serializer = UsuarioSerializer(data=request.data)
         if serializer.is_valid():
-            # Se extraen los datos validados del serializer
             email = serializer.validated_data.get('email')
             password = serializer.validated_data.get('password')
             nombre = serializer.validated_data.get('nombre')
             apellido = serializer.validated_data.get('apellido')
             id_rol_id = serializer.validated_data.get('id_rol_id')
 
-            # Verificar si el usuario ya existe en la base de datos
             usuario_existente = Usuario.objects.filter(email=email).exists()
             if usuario_existente:
-                # Si el usuario ya existe, se retorna un mensaje de error y un código de estado 400 (Solicitud incorrecta)
                 return Response({'mensaje': 'El correo electrónico ya está registrado'}, status=400)
 
-            # Crear un nuevo usuario en la base de datos
-            usuario = Usuario.objects.create_user(email = email, password = password, nombre = nombre, apellido = apellido, id_rol_id = id_rol_id)
+            usuario = Usuario.objects.create_user(email=email, password=password, nombre=nombre, apellido=apellido, id_rol_id=id_rol_id)
 
-            # Generar un token JWT (JSON Web Token)
-            token_payload = {'user_id': usuario.id_usuario, 'email': email, 'nombre': nombre}
-            token = jwt.encode(token_payload, settings.SECRET_KEY, algorithm='HS256')
+            # Generar el token JWT
+            expiration_time = timezone.now() + datetime.timedelta(hours=12)
+            expiration_timestamp = int(expiration_time.timestamp())
 
-            # Se retorna una respuesta con un mensaje de éxito, el token y un código de estado 201 (Creado)
+            payload = {
+                'id_usuario': usuario.id_usuario,
+                'email': email,
+                'nombre': usuario.nombre,
+                'exp': expiration_timestamp
+            }
+            token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
             return Response({'mensaje': 'Registro exitoso', 'token': token}, status=201)
         else:
-            # Si los datos no son válidos, se retorna un mensaje de error con los errores del serializer y un código de estado 400 (Solicitud incorrecta)
             return Response({'mensaje': 'Datos no válidos', 'errores': serializer.errors}, status=400)
-
-    @staticmethod
-    @csrf_exempt
-    
-    def inicio_sesion(request):
+        
+    def inicio_sesion(self, request):
         if request.method == 'POST':
             try:
-                # Se obtienen los datos de inicio de sesión del cuerpo de la solicitud
-                data = json.loads(request.body)
+                data = request.data
                 email = data.get('email', '')
                 password = data.get('password', '')
 
-                # Se validan las credenciales del usuario utilizando la función authenticate de Django
                 usuario = authenticate(request, email=email, password=password)
 
                 if usuario is not None:
-                    # Si las credenciales son válidas, se genera un token JWT (JSON Web Token) con tiempo de expiración
                     expiration_time = timezone.now() + datetime.timedelta(hours=12)
                     expiration_timestamp = int(expiration_time.timestamp())
 
@@ -77,27 +69,19 @@ class UsuarioView(APIView):
                     }
                     token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
 
-                    # Se retorna una respuesta JSON con un mensaje de éxito, el token y un código de estado 200 (Éxito)
-                    return JsonResponse({'mensaje': 'Inicio de sesión exitoso', 'token': token}, status=200)
+                    return Response({'mensaje': 'Inicio de sesión exitoso', 'token': token}, status=200)
                 else:
-                    # Si las credenciales son inválidas, se retorna un mensaje de error y un código de estado 401 (No autorizado)
-                    return JsonResponse({'mensaje': 'Credenciales inválidas'}, status=401)
-            except json.JSONDecodeError:
-                # Si los datos no son válidos, se retorna un mensaje de error y un código de estado 400 (Solicitud incorrecta)
-                return JsonResponse({'mensaje': 'Datos no válidos'}, status=400)
+                    return Response({'mensaje': 'Credenciales inválidas'}, status=401)
+            except:
+                return Response({'mensaje': 'Datos no válidos'}, status=400)
 
-        # Si el método de la solicitud no es POST, se retorna un mensaje de error y un código de estado 405 (Método no permitido)
-        return JsonResponse({'mensaje': 'Método no permitido'}, status=405)
+        return Response({'mensaje': 'Método no permitido'}, status=405)
 
-    @staticmethod
-    @csrf_exempt
-    def lista_usuarios(request):
-        # Se obtienen todos los usuarios de la base de datos
+    def list_users(self, request):
         usuarios = Usuario.objects.all()
-        # Se crea una lista de diccionarios con los datos de cada usuario
-        usuarios_data = [{'id_usuario': usuario.id_usuario,'nombre': usuario.nombre, 'apellido': usuario.apellido, 'email': usuario.email} for usuario in usuarios]
-        # Se retorna una respuesta JSON con la lista de usuarios y un código de estado 200 (Éxito)
-        return JsonResponse({'usuarios': usuarios_data}, status=200)
+        serializer = UsuarioSerializer(usuarios, many=True)
+        return Response(serializer.data, status=200)
+    
 #===========================================================================================================================================================================    
 
 
